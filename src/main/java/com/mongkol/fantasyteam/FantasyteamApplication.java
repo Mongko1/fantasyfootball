82a320b59696b3440feb98e6a15f238b0a3b056b
Mongko1 <mongkol.puetthong@gmail.com>
2025-03-16 04:42:28 +0700

    initial commit

---
+++ src/main/java/com/mongkol/fantasyteam/FantasyteamApplication.java
@@ -0,0 +1,13 @@
+package com.mongkol.fantasyteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyteamApplication.class, args);
+	}
+
+}
